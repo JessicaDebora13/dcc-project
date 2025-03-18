48b24743f7840c1e259cc456c74359d8a7f49f19
lizbethlulu <catheline018.email@example.com>
2025-03-18 21:40:45 +0700

    Menambahkan Dockerfile2 dan mengubah app.py

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Define the /hello route
-@app.route('/hello')
-def hello():
-    return "Hello, World!"
+# Route untuk hello dengan nama pengguna di URL
+@app.route('/hello/<username>')
+def hello(username):
+    return f"Hello, {username}!"
 
-# Define the /goodbye route
-@app.route('/goodbye')
-def goodbye():
-    return "Goodbye, World!"
+# Route untuk goodbye dengan nama pengguna di URL
+@app.route('/goodbye/<username>')
+def goodbye(username):
+    return f"Goodbye, {username}!"
 
 if __name__ == '__main__':
-    # Start the Flask web server on port 5000
-    app.run(debug=True, port=5000)
+    # Start the Flask web server on port 8080
+    app.run(debug=True, port=8080)
